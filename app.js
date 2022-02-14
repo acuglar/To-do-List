f94804503390fead0d8521688ca33ba3e06f1267
Alexandre Uglar <acuglar@gmail.com>
2022-02-14 13:16:51 -0300

    refactor: implementando outra estrategia de rm para eliminar dependencia da marcaçao HTML estatica

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ formAddTodo.addEventListener('submit', (event) => {
 	const inputValue = event.target.add.value.trim();
 	if (inputValue.length) {
 		todosContainer.innerHTML += `
-      <li class="list-group-item d-flex justify-content-between align-items-center">
+      <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
         <span>${inputValue}</span>
-        <i class="far fa-trash-alt delete"></i>
+        <i class="far fa-trash-alt" data-trash="${inputValue}"></i>
       </li>
     `;
 		event.target.reset();
@@ -20,8 +20,8 @@ formAddTodo.addEventListener('submit', (event) => {
 todosContainer.addEventListener('click', (event) => {
 	const clickedElement = event.target;
 
-	if (Array.from(clickedElement.classList).includes('delete')) {
-		clickedElement.parentElement.remove();
+	if (clickedElement.dataset.trash) {
+		document.querySelector(`[data-todo="${clickedElement.dataset.trash}"]`).remove();
 	}
 });
 
